88c0f7019da57993c2f7b0631849e98778aeed07
SureshPradhana <sureshpradhana19@gmail.com>
2024-06-05 14:29:03 +0530

    chore:ui

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -1,4 +1,18 @@
-<div class="flex">
-	<img src="favicon.png" alt="Missing" />
-	<h1 class="m-1 font-sans text-4xl font-bold">Missing</h1>
+<script lang="ts">
+	import { AspectRatio } from '$lib/components/ui/aspect-ratio';
+	import { Hamburger } from 'svelte-hamburgers';
+	export let open;
+</script>
+
+<div class="m-4 flex justify-between">
+	<div class="flex items-center space-x-4">
+		<div class="w-14">
+			<AspectRatio ratio={16 / 16} class="">
+				<img src="favicon.png" alt="Missing" class="rounded-md object-cover" />
+			</AspectRatio>
+		</div>
+		<!-- <h1 class="m-1 font-sans text-4xl font-bold">Missing</h1> -->
+	</div>
+
+	<Hamburger bind:open />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Scene from '$lib/Scene.svelte';
 	import { Canvas } from '@threlte/core';
+
+	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
@@ -25,72 +27,63 @@
 	];
 </script>
 
-<div><Canvas><Scene></Scene></Canvas></div>
+<div class="grid justify-center">
+	<!-- <div><Canvas><Scene></Scene></Canvas></div> -->
 
-<Button href="/find">find</Button>
+	<Button href="/find" class="m-1 w-max justify-self-end px-16">find</Button>
 
-<Carousel.Root class="w-full max-w-xs">
-	<Carousel.Content>
-		{#each Array(5) as _, i (i)}
-			<Carousel.Item>
-				<div class="p-1">
-					<Card.Root>
-						<Card.Content class="flex h-full flex-col">
-							<AspectRatio ratio={16 / 9} class="bg-muted">
-								<img
-									src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-									alt="Gray by Drew Beamer"
-									class="h-full w-full rounded-md object-cover"
-								/>
-							</AspectRatio>
-							<span class="text-4xl font-semibold">{i + 1}</span>
+	<Carousel.Root class="w-full max-w-5xl sm:max-w-xl md:max-w-3xl lg:max-w-5xl">
+		<Carousel.Content>
+			{#each Array(5) as _, i (i)}
+				<Carousel.Item>
+					<div class="p-1">
+						<Card.Root>
+							<Card.Content class="flex h-full flex-col">
+								<AspectRatio ratio={16 / 9} class="my-2">
+									<img
+										src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+										alt="Gray by Drew Beamer"
+										class="  h-full
+								w-full rounded-md border-2 border-black object-cover"
+									/>
+								</AspectRatio>
 
-							<Card.Root>
-								<Card.Header>
-									<Card.Title>Notifications</Card.Title>
-									<Card.Description>You have 3 unread messages.</Card.Description>
-								</Card.Header>
-								<Card.Content class="grid gap-4">
-									<div class="flex items-center space-x-4 rounded-md border p-4">
-										<Bell />
-										<div class="flex-1 space-y-1">
-											<p class="text-sm font-medium leading-none">Push Notifications</p>
-											<p class="text-sm text-muted-foreground">Send notifications to device.</p>
-										</div>
-										<Switch />
-									</div>
-									<div>
-										{#each notifications as notification, idx (idx)}
-											<div
-												class="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-											>
-												<span class="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-												<div class="space-y-1">
-													<p class="text-sm font-medium leading-none">
-														{notification.title}
-													</p>
-													<p class="text-sm text-muted-foreground">
-														{notification.description}
-													</p>
+								<Card.Root>
+									<Card.Header>
+										<Card.Title>Notifications</Card.Title>
+										<Card.Description>You have 3 unread messages.</Card.Description>
+									</Card.Header>
+									<Card.Content class="grid gap-4">
+										<div>
+											<div class="flex items-center space-x-4">
+												<div class="space-y-2">
+													<Skeleton class="h-4 w-96" />
+													<Skeleton class="h-4 w-96" />
+													<Skeleton class="h-4 w-96" />
+													<Skeleton class="h-4 w-96" />
+													<Skeleton class="h-4 w-96" /><Skeleton class="h-4 w-96" />
+													<Skeleton class="h-4 w-96" /><Skeleton class="h-4 w-96" />
+													<Skeleton class="h-4 w-96" /><Skeleton class="h-4 w-96" />
+													<Skeleton class="h-4 w-96" /><Skeleton class="h-4 w-96" />
 												</div>
 											</div>
-										{/each}
-									</div>
-								</Card.Content>
-								<Card.Footer>
-									<Button class="w-full">
-										<Check class="mr-2 h-4 w-4" /> Mark all as read
-									</Button>
-								</Card.Footer>
-							</Card.Root>
-						</Card.Content>
-					</Card.Root>
-				</div>
-			</Carousel.Item>
-		{/each}
-	</Carousel.Content>
-	<Carousel.Previous />
-	<Carousel.Next />
-</Carousel.Root>
+										</div>
+									</Card.Content>
+									<Card.Footer>
+										<Button class="w-full">
+											<Check class="mr-2 h-4 w-4" /> Mark all as read
+										</Button>
+									</Card.Footer>
+								</Card.Root>
+							</Card.Content>
+						</Card.Root>
+					</div>
+				</Carousel.Item>
+			{/each}
+		</Carousel.Content>
+		<Carousel.Previous />
+		<Carousel.Next />
+	</Carousel.Root>
+</div>
 
 <style></style>
